caae4f0c037f7b628c03d655f2dda932c0018dc4
Karan Parashar <karan@pixelos.net>
2024-05-23 14:43:48 +0530

    Implement endpoints for tag management

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask
 
 from modules.pic_manage.routes import pic_manage_bp
+from modules.tag_manage.routes import tag_manage_bp
 
 app = Flask(__name__)
 
 app.register_blueprint(pic_manage_bp, url_prefix='/api')
+app.register_blueprint(tag_manage_bp, url_prefix='/api')
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ modules/tag_manage/routes.py
@@ -0,0 +1,46 @@
+from config import db_connection
+from flask import Blueprint, jsonify
+
+tag_manage_bp = Blueprint('tag_manage', __name__)
+
+db = db_connection()
+
+
+@tag_manage_bp.route('/tags/<user_id>', methods=['GET'])
+def get_tags(user_id):
+    cursor = db.cursor()
+    cursor.execute(
+        "SELECT tag_id, tag_name FROM tags WHERE user_id = %s", (user_id))
+    tags = cursor.fetchall()
+    cursor.close()
+
+    tag_list = []
+    for tag in tags:
+        tag_list.append({
+            'tag_id': tag[0],
+            'tag_name': tag[1]
+        })
+
+    return jsonify({
+        'tags': tag_list
+    }), 200
+
+
+@tag_manage_bp.route('/tags/<photo_id>', methods=['GET'])
+def get_photo_tag(photo_id):
+    cursor = db.cursor()
+    # todo: fetch list of all tags for a picture
+    # cursor.execute()
+    tags = cursor.fetchall()
+    cursor.close()
+
+    tag_list = []
+    for tag in tags:
+        tag_list.append({
+            'tag_id': tag[0],
+            'tag_name': tag[1]
+        })
+
+    return jsonify({
+        'tags': tag_list
+    }), 200
