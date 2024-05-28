6b2a9ee2a5a7983eb0398528995b5d0dec8247dd
Karan Parashar <karan@pixelos.net>
2024-05-29 03:16:35 +0530

    Add endpoint for retrieving pictures from a tag

--- src/routes/tags.py
+++ src/routes/tags.py
@@ -5,6 +5,7 @@ from src.utils.auth_utils import token_required
 tags_bp = Blueprint('tag_manage', __name__)
 
 
+# retrieves available tags for a user
 @tags_bp.route('/tags/user/<username>', methods=['GET'])
 @token_required
 def get_tags(username):
@@ -26,6 +27,33 @@ def get_tags(username):
     }), 200
 
 
+# retrieves all pictures associated with a particular tag
+@tags_bp.route('/photo/tags/<tag_id>', methods=['GET'])
+@token_required
+def get_photos_by_tag(tag_id):
+    try:
+        cursor = db.cursor()
+        cursor.execute("""
+            SELECT p.photo_path 
+            FROM photos p 
+            INNER JOIN photo_tags pt ON p.photo_id = pt.photo_id 
+            WHERE pt.tag_id = %s
+        """, (tag_id,))
+        photo_paths = cursor.fetchall()
+        cursor.close()
+
+        photos = [photo['photo_path'] for photo in photo_paths]
+
+        return jsonify({
+            'photos': photos
+        }), 200
+    except Exception as e:
+        return jsonify({
+            'message': f'Error retrieving photos: {e}'
+        }), 500
+
+
+# retrieves all tag_id from a particular picture
 @tags_bp.route('/tags/photo/<photo_id>', methods=['GET'])
 @token_required
 def get_photo_tags(photo_id):
