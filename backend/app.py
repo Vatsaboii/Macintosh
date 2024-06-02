bbeef1e396895296b7ccb9fba91da6ea3792d7f8
Karan Parashar <karan@pixelos.net>
2024-06-03 00:51:41 +0530

    backend: Add cors support

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,13 @@
 from flask import Flask
+from flask_cors import CORS
 
 from src.routes.auth import auth_bp
 from src.routes.pictures import pictures_bp
 from src.routes.tags import tags_bp
 
 app = Flask(__name__)
+CORS(app)
+
 
 app.register_blueprint(auth_bp, url_prefix='/api')
 app.register_blueprint(pictures_bp, url_prefix='/api')
